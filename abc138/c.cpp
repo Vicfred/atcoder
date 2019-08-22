8863ce9945682033da3fc5b26f08adbd60d83c2a
Vicfred <vicfredboy@gmail.com>
2019-08-22 18:34:44 -0500

    sorting

---
+++ abc138/c.cpp
@@ -0,0 +1,33 @@
+// C - Alchemist
+// Sorting
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<long double> V(N,0.0L);
+    for(auto &idx: V)
+        cin >> idx;
+
+    long double current = 0.0L;
+
+    do {
+        sort(V.begin(), V.end(), greater<int>());
+
+        current = (V[V.size()-2]+V[V.size()-1])/2;
+
+        V.pop_back();
+        V.pop_back();
+
+        V.push_back(current);
+    } while(V.size() > 1);
+
+    cout << V[0] << endl;
+
+    return 0;
+}
