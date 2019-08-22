14e0fa153f69e117f9f417f9461205ce1b505b2e
Vicfred <vicfredboy@gmail.com>
2019-08-22 18:04:44 -0500

    IO

---
+++ abc138/a.cpp
@@ -0,0 +1,18 @@
+// A - Red or Not
+// IO
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a;
+    string s;
+    cin >> a >> s;
+    if(a < 3200)
+        cout << "red" << endl;
+    else
+        cout << s << endl;
+    return 0;
+}
+
